185b94ec3d4a5c13dd2b572cfe50b9181b63de38
Daniel <dmartinez@stackbuilders.com>
2026-08-26 16:28:07 -0500

    Add Pet_project_FP bookList.js: avoid mutation by returning new arrays in add/remove

---
+++ Pet_project_FP/bookList.js
@@ -0,0 +1,16 @@
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(list, bookName) {
+  return [...list, bookName];
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(list, bookName) {
+  return list.filter(book => book !== bookName);
+
+    // Change code above this line
+}
